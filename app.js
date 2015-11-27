b1398c1cebf94101907aa8cedda29b6119e0daed
Kazumasa KUMAMOTOF29 <kumamotone@gmail.com>
2015-11-27 16:40:46 +0900

    不要なコメントを削除

--- app.js
+++ app.js
@@ -1,41 +1,7 @@
 console.time('timer');
-// jsinq をロード
 require('./jsinq');
 require('./jsinq-query');
- /* 
-  var SparqlParser = require('./SPARQL.js/').Parser;
-  var parser = new SparqlParser();
-  var parsedQuery = parser.parse(
-  'PREFIX foaf:    <http://xmlns.com/foaf/0.1/>'+
-  'SELECT ?name ?mbox'+
-  '{  ?x foaf:name ?name .'+
-  '  ?x foaf:mbox ?mbox .'+
-  '  FILTER (?name > 50 ) }');
-  
-  console.log(parsedQuery);
 
- for( var i in parsedQuery.where ){
-    if( parsedQuery.where[i].type == 'filter' ) { 
-      exp = parsedQuery.where[i].expression;
-      console.log(exp);
-    }
-  }
-  
-   parsedQuery.where.push({"type":"filter","expression":{"type":"operation","operator":"regex","args":["?name","\"Smi\""]}});
-  // console.log(parsedQuery);
-  // console.log(parsedQuery.where[1].expression);
-  // console.log(parsedQuery.where[1].expression.args);
-
-  // parsedQuery.where[1].expression.args.push();
-
-   var SparqlGenerator = require('./SPARQL.js/').Generator;
-   var generator = new SparqlGenerator();
-   parsedQuery.variables.push('?mickey');
-   var generatedQuery = generator.stringify(parsedQuery);
-
-  console.log(generatedQuery);
-*/
-  // LINQコード
   var querystr = ' \
     from product in $0 \
     join feature in $1  \
