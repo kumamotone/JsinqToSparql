442c40cd934c0269aa72178e1eac20ad9070e561
Kazumasa KUMAMOTOF29 <kumamotone@gmail.com>
2015-11-30 18:50:27 +0900

    app_minimal の出力先を output に output に gitkeep 追加

--- app_minimal.js
+++ app_minimal.js
@@ -1,22 +1,18 @@
 console.time('timer');
 require('./jsinq');
 require('./jsinq-query');
+var fs = require('fs');
 
 var querystr = ' \
   from product in $0 \
-  select [ product.prdct, product.prdctlbl ]  \
+  select [product.prdct, product.prdctlbl, product.value2, product.text1, product.text2, product.text3, \
+          product.pdate ]  \
 ';
   
 console.log("LINQ Query:" + querystr);
 var query = new jsinq.Query(querystr);
 query.executeQuery(query,
-function (values) {
-  //console.log("Results:");
-  for (var key in values) {
-    console.log(key + ': ' + values[key]);
-  }
+function (retval) {
+  fs.writeFile('outputs/outputs.json', JSON.stringify(retval));
   console.timeEnd('format');
-  // console.timeEnd('timer');
 });
-
-// console.log(query.getQueryFunction().toString());
