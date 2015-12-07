b830caea1ad96a8c0c299c17dc0d191bae5c2fbf
Kazumasa KUMAMOTOF29 <kumamotone@gmail.com>
2015-12-07 19:41:23 +0900

    テスト用クエリの追加

--- app.js
+++ app.js
@@ -4,17 +4,37 @@ require('./jsinq');
 require('./jsinq-query');
 var fs = require('fs');
 
+/* full query (using 3 tables) */
+var querystr = ' \
+    from product in $0 \
+    join feature in $1  \
+    on product.prdctft equals feature.ft \
+    join producttype in $2  \
+    on product.ptype equals producttype.pt \
+    where product.value1 < 400 \
+    select [product.prdct, product.prdctlbl, product.value2, product.text1, product.text2, product.text3, \
+           product.pdate, feature.ft, feature.fdate, producttype.ptlbl ]  \
+';
+
+/* join 2 tables */
+/*
 var querystr = ' \
   from product in $0 \
-  join feature in $1  \
-  on product.prdctft equals feature.ft \
-  join producttype in $2  \
+  join producttype in $1  \
   on product.ptype equals producttype.pt \
-  where product.value1 < 400 \
-  select [product.prdct, product.prdctlbl, product.value2, product.text1, product.text2, product.text3, \
-          product.pdate, feature.ft, feature.ftct, feature.fdate, producttype.ptlbl ]  \
+  select [product.prdct, product.prdctlbl, product.value1, product.ptype, producttype.pt, producttype.ptlbl ]  \
 ';
-  
+*/
+
+/* select only */
+/*
+var querystr = ' \
+               from product in $0 \
+               select [product.prdct, product.prdctlbl, product.value1, product.ptype ] \
+               ';
+*/
+
+
 console.log("===== Given LINQ Query ====")
 console.log(querystr + "\n");
 var query = new jsinq.Query(querystr);
