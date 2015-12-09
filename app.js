f37d910b008ee3be8d84da46413b3435ae8ce2e4
Kazumasa KUMAMOTOF29 <kumamotone@gmail.com>
2015-12-09 12:00:53 +0900

    edit exp query

--- app.js
+++ app.js
@@ -5,26 +5,28 @@ require('./jsinq-query');
 var fs = require('fs');
 
 /* full query (using 3 tables) */
-var querystr = ' \
-    from product in $0 \
-    join feature in $1  \
-    on product.prdctft equals feature.ft \
-    join producttype in $2  \
-    on product.ptype equals producttype.pt \
-    where product.value1 < 400 \
-    select [product.prdct, product.prdctlbl, product.value2, product.text1, product.text2, product.text3, \
-           product.pdate, feature.ft, feature.fdate, producttype.ptlbl ]  \
-';
-
-/* join 2 tables */
 /*
 var querystr = ' \
-  from product in $0 \
-  join producttype in $1  \
-  on product.ptype equals producttype.pt \
-  select [product.prdct, product.prdctlbl, product.value1, product.ptype, producttype.pt, producttype.ptlbl ]  \
+    from product in $0 \
+    join producttype in $1 \
+    on product.ptype equals producttype.pt \
+    where product.value1 < 400 \
+    select [product.prdct, product.prdctlbl, product.value2, product.ptype, \
+           product.pdate, producttype.pt, producttype.ptlbl ]  \
 ';
 */
+
+/* join 2 tables */
+
+var querystr = ' \
+  from product in $0 \
+  join feature in $1 \
+  on product.prdctft equals feature.ft \
+  join producttype in $2  \
+  on product.ptype equals producttype.pt \
+  select [product.prdct, product.prdctlbl, product.value1, product.ptype, feature.ft, producttype.pt, producttype.ptlbl ]  \
+';
+
 
 /* select only */
 /*
