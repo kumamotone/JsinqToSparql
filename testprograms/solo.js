e7654a7f0127352f01e55373a22e1ef75c2871a7
Kazumasa KUMAMOTOF29 <kumamotone@gmail.com>
2015-12-20 08:03:03 +0900

    solo.js 実験と等価なSPARQLクエリ 12/20 版

--- testprograms/solo.js
+++ testprograms/solo.js
@@ -1,33 +1,30 @@
 console.time('ready');
 var SparqlClient = require('sparql-client');
 var util = require('util');
-var endpoint = 'http://192.168.225.129:8890/sparql';
+var endpoint = 'http://130.158.76.30:8890/sparql';
 
 // Get the leaderName(s) of the given citys
 // if you do not bind any city, it returns 10 random leaderNames
 var query = " \
-DEFINE get:refresh \"0\" \
-DEFINE get:soft \"replace\" \
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \
-PREFIX bsbm: <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/> \
-PREFIX dc: <http://purl.org/dc/elements/1.1/>  \
-SELECT ?prdct ?prdctlbl ?value ?text1 ?text2 ?text3 ?pdate ?ftlbl ?ftct ?fdate \
-WHERE {\
-  ?prdct rdf:type bsbm:Product .\
-  ?prdct rdfs:label ?prdctlb .\
-  ?prdct bsbm:productFeature ?prdctft .\
-  ?prdct bsbm:productPropertyNumeric1 ?value1 . \
-  ?prdct bsbm:productPropertyNumeric2 ?value2 . \
-  ?prdct bsbm:productPropertyTextual1 ?text1 . \
-  ?prdct bsbm:productPropertyTextual2 ?text2 . \
-  ?prdct bsbm:productPropertyTextual3 ?text3 . \
-  ?prdct dc:date ?pdate . \
-  ?prdctft rdf:type bsbm:ProductFeature  .\
-  ?prdctft rdfs:label ?ftlbl .\
-  ?prdctft rdfs:comment ?ftct .\
-  ?prdctft dc:date ?fdate . \
-  FILTER (?value1 < 400) . \
+DEFINE get:refresh \"0\" DEFINE get:soft \"replace\"\
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\
+PREFIX bsbm: <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/>\
+PREFIX dc: <http://purl.org/dc/elements/1.1/>\
+SELECT ?prdct ?prdctlbl ?value1 ?pt ?ft ?ftlbl ?ptct ?pdate WHERE {\
+  ?prdct <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/Product>.\
+  ?prdct <http://www.w3.org/2000/01/rdf-schema#label> ?prdctlbl.\
+  ?prdct <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/productFeature> ?ft.\
+  ?prdct <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?pt.\
+  ?prdct <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/productPropertyNumeric1> ?value1.\
+  ?prdct <http://purl.org/dc/elements/1.1/date> ?pdate.\
+  ?ft <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/ProductFeature>.\
+  ?ft <http://www.w3.org/2000/01/rdf-schema#label> ?ftlbl.\
+  ?pt <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/ProductType>.\
+  ?pt <http://www.w3.org/2000/01/rdf-schema#label> ?ptlbl.\
+  ?pt <http://www.w3.org/2000/01/rdf-schema#comment> ?ptct.\
+  ?pt <http://purl.org/dc/elements/1.1/date> ?ptdate.\
+  FILTER(?value1 < 200)\
 }\
 ";
 
@@ -36,6 +33,7 @@ function timerCallback()
   console.timeEnd('timerForSPARQL');
 }
 
+var fs = require('fs');
 var client = new SparqlClient(endpoint);
 console.log("Query to " + endpoint);
 console.log("Query: " + query);
@@ -47,7 +45,8 @@ client.query(query)
   //.bind('city', 'db:Casablanca')
 //  .bind('city', '<http://dbpedia.org/resource/Vienna>')
   .execute(function(error, results) {
-  process.stdout.write(util.inspect(arguments, null, 20, true)+"\n");1
+  fs.writeFile('outputs/solooutputs.json', JSON.stringify(results));
+    //process.stdout.write(util.inspect(arguments, null, 20, true)+"\n");1
   timerCallback();
 });
 
