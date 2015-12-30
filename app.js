ecace2924a644358ae12c39ce567a5f7e09df6d9
Kazumasa KUMAMOTOF29 <kumamotone@gmail.com>
2015-12-30 17:25:50 +0900

    add producer,offer view

--- app.js
+++ app.js
@@ -21,13 +21,18 @@ var querystr = ' \
 
 var querystr = ' \
   from product in $0 \
-  join feature in $1 \
+  join offer in $1 \
+  on product.prdct equals offer.ofprdct \
+   join feature in $2 \
   on product.prdctft equals feature.ft \
-  join producttype in $2  \
+  join producttype in $3  \
   on product.ptype equals producttype.pt \
-  where product.value1 < 200 \
+  join producer in $4 \
+  on product.pd equals producer.pd \
+ where product.value1 < 200 \
+  where offer.ofdays > 5 \
   select [product.prdct, product.prdctlbl, product.value1, product.ptype, product.ptct, product.pdate, \
-    feature.ft, feature.ftlbl, producttype.pt, producttype.ptlbl, producttype.ptdate ]  \
+    feature.ft, feature.ftlbl, producttype.pt, producttype.ptlbl, producttype.ptdate, producer.pd, producer.pdcountry, offer.of, offer.ofdays, offer.ofdate ]  \
 ';
 
 
